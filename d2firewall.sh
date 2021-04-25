82c2dcf9b7474d37151341faf68617bd0aa5fc8f
Cloud <cloudex99@users.noreply.github.com>
2021-04-24 22:56:16 -0400

    Update d2firewall.sh

--- d2firewall.sh
+++ d2firewall.sh
@@ -16,8 +16,10 @@ reset_ip_tables () {
   sudo iptables -P FORWARD ACCEPT
   sudo iptables -P OUTPUT ACCEPT
 
-  #sudo iptables -t nat -F
-  #sudo iptables -t mangle -F
+  #comment out the next 2 lines if you are not using a vps
+  sudo iptables -t nat -F
+  sudo iptables -t mangle -F
+  
   sudo iptables -F
   sudo iptables -X
 
