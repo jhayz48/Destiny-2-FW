25cd3eae33611fc0095c6e86c34c82c34899f49c
Cloud <cloudex99@users.noreply.github.com>
2021-04-28 00:23:29 -0400

    Update d2firewall.sh

--- d2firewall.sh
+++ d2firewall.sh
@@ -67,7 +67,7 @@ setup () {
   echo $net >> /tmp/data.txt
 
   ids=()
-  read -p "Would you like to sniff the ID automatically?(psn/xbox only) y/n: " yn
+  read -p "Would you like to sniff the ID automatically?(psn/xbox/steam) y/n: " yn
   yn=${yn:-"y"}
   if ! [[ $platform =~ ^(psn|xbox|steam)$ ]]; then
     yn="n"
